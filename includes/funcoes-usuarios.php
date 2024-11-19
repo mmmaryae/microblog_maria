c522fd9c215d1c51e80a2c4f2624e56772ea5bf1
maria <150175935+mmmaryae@users.noreply.github.com>
2024-11-19 14:45:59 -0300

    mysqli_fetch_all (todos) //mysqli_fetch_assoc (um)

--- includes/funcoes-usuarios.php
+++ includes/funcoes-usuarios.php
@@ -24,3 +24,15 @@ function listarUsuarios($conexao){
     return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 }
 
+// mysqli_fetch_all (todos)
+//mysqli_fetch_assoc (um) 
+//SELECT    para carregar dados de uma PESSOA através do ID
+function listarUmUsuario($conexao, $id){
+$sql = "SELECT * FROM usuarios WHERE id = $id";
+
+//Execução e verificação do comando SQL 
+$resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+//Extração dos dados de UMA PESSOA como Array Associativo
+return mysqli_fetch_assoc($resultado);
+}
+
