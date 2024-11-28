03a972296692356b0705a33e2fa5d0ae1827ec88
maria <150175935+mmmaryae@users.noreply.github.com>
2024-11-28 14:01:47 -0300

    comentarios do delete

--- admin/noticia-exclui.php
+++ admin/noticia-exclui.php
@@ -3,9 +3,21 @@
 require "../includes/funcoes-noticias.php";
 require "../includes/funcoes-controle-de-acesso.php";
 
+
+//Verificar se o usuario está logado
 verificarAcesso();
+
+//Capturando o id da noticia que será DELETADA 
 $idNoticia = $_GET['id'];
+
+//Capturando o id do usuário logado na sessão
 $idUsuario =$_SESSION['id'];
+
+//Capturando o tipo de usuario logado na sessão 
 $tipoUsuario = $_SESSION['tipo'];
+
+//Execultando o delete da noticia no banco de dados
 excluirNoticia($conexao, $idNoticia, $idUsuario, $tipoUsuario);
+
+//Redirecionando para a pagina com a lista de noticias
 header("location:noticias.php");                   
